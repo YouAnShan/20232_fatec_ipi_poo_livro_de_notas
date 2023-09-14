26d5fd61758ee24723ffdc212691428ca1ba5e43
Aluno <aluno@IPIRANGA.FATEC>
2023-09-14 16:07:11 -0300

    ap06(java): fim do passo 12.1

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,6 +1,16 @@
-public class LivroDeNotas{
-    public void exibirMensagem(){
-        System.out.println("Bem vindo ao livro de notas");
+public class LivroDeNotas{ //assinatura da classe
+
+    //variável de instância
+    //instância é sinônimo de objeto
+    String nomeDisciplina;
+    public void exibirMensagem(){ //assinatura do método
+        System.out.printf("Bem vindo ao livro de notas de %s\n", nomeDisciplina);
     } 
+
+    //construtor padrão (aquele cuja a lista de parâmetros é vazia)
+    LivroDeNotas(String nomeDisciplina){
+        // shadowing
+        this.nomeDisciplina = nomeDisciplina;
+    }
 
 }
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -1,10 +1,12 @@
 public class TesteLivroDeNotas{
-    static public void main(String args[]){
-        //variável de referência
-        //malloc
-        LivroDeNotas livroDeNotas = new LivroDeNotas();
-        //Tipo nomeDaClasse = (atribuição) OperadorDeConstruçãoDeObjeto 
+    static public void main(String args []){
+        //nome completamente qualificado da classe
+        String nomeDisciplina = javax.swing.JOptionPane.showInputDialog("Digite o nome da disciplina");
+        //inferência de tipo
+        var livroDeNotas = new LivroDeNotas(nomeDisciplina);
+        //livroDeNotas.nomeDisciplina = nomeDisciplina;
+        //enviando a mensagem "exibirMensagem" ao objeto livroDeNotas
         livroDeNotas.exibirMensagem();
-        System.out.println("Até logo");
+        System.out.println("Ate logo");
     }
 }
